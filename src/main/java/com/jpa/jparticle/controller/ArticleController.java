df085173877059b9c35a8d4d491d62ed004599d2
devkwonms <alstj1379@nate.com>
2022-11-29 19:48:43 +0900

    form data를 받기위한 PostMapping(Create) method 생성

--- src/main/java/com/jpa/jparticle/controller/ArticleController.java
+++ src/main/java/com/jpa/jparticle/controller/ArticleController.java
@@ -2,6 +2,7 @@ package com.jpa.jparticle.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
@@ -10,5 +11,10 @@ public class ArticleController {
     public String newArticleForm() {
 
         return "articles/new";
+    }
+
+    @PostMapping("/articles/create")
+    public String createArticle() {
+        return "";
     }
 }
