2516b35583d5238d3956ea69ef37e33695c64f49
devkwonms <alstj1379@nate.com>
2022-11-29 15:18:03 +0900

    들여쓰기 정리

--- src/main/java/com/jpa/jparticle/controller/FirstController.java
+++ src/main/java/com/jpa/jparticle/controller/FirstController.java
@@ -1,11 +1,15 @@
 package com.jpa.jparticle.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 @Controller
 public class FirstController {
+
     @GetMapping("/hi")
-    public String niceToMeetYou() {
+    public String niceToMeetYou(Model model) {
+        model.addAttribute("username", "hongpark");
         return "greetings";
     }
 }
