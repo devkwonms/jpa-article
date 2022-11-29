64442be47097e314d9e7c523e390fe56f410cdd5
devkwonms <alstj1379@nate.com>
2022-11-29 14:05:41 +0900

    first setting

---
+++ src/main/java/com/jpa/jparticle/JparticleApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.jparticle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JparticleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JparticleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpa/jparticle/JparticleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.jparticle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JparticleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
