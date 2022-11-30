2e40a55285d1806e6275c3649c762921f94d1919
devkwonms <alstj1379@nate.com>
2022-11-30 16:12:47 +0900

    수정하기위한 Getmapping method추가

--- src/main/java/com/jpa/jparticle/controller/ArticleController.java
+++ src/main/java/com/jpa/jparticle/controller/ArticleController.java
@@ -72,4 +72,14 @@ public class ArticleController {
         return "articles/index";
     }
 
+    @GetMapping("/articles/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) {
+        // 수정할 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        // 모델에 데이터 등록
+        model.addAttribute("article", articleEntity);
+        // 뷰 페이지 설정
+        return "articles/edit";
+    }
+
 }
