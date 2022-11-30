ed2ed123752088da0135e3bf9f9a40d1e8691ffa
devkwonms <alstj1379@nate.com>
2022-11-30 20:10:52 +0900

    댓글 목록 조회 - CommentApiController

--- src/main/java/com/jpa/jparticle/api/CommentApiController.java
+++ src/main/java/com/jpa/jparticle/api/CommentApiController.java
@@ -1,8 +1,15 @@
 package com.jpa.jparticle.api;
 
+import com.jpa.jparticle.dto.CommentDto;
 import com.jpa.jparticle.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CommentApiController {
@@ -11,6 +18,13 @@ public class CommentApiController {
     private CommentService commentService;
 
     // 댓글 목록 조회
+    @GetMapping("/api/articles/{articleId}/comments")
+    public ResponseEntity<List<CommentDto>> comments(@PathVariable Long articleId) {
+        // 서비스에게 위임
+        List<CommentDto> dtos = commentService.comments(articleId);
+        // 결과 응답
+        return ResponseEntity.status(HttpStatus.OK).body(dtos);
+    }
 
     // 댓글 생성
 
