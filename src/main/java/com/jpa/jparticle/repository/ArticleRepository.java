2e04335fee9c9cdfba6ffc6fca8c0479749fa255
devkwonms <alstj1379@nate.com>
2022-11-30 15:45:14 +0900

    repository.findAll() 의 return타입 overriding

--- src/main/java/com/jpa/jparticle/repository/ArticleRepository.java
+++ src/main/java/com/jpa/jparticle/repository/ArticleRepository.java
@@ -3,5 +3,9 @@ package com.jpa.jparticle.repository;
 import com.jpa.jparticle.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface ArticleRepository extends CrudRepository<Article,Long> {
+    @Override
+    ArrayList<Article> findAll();
 }
