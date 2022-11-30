e521c389f287fa63a2f847702eb9d2e23884ae0f
devkwonms <alstj1379@nate.com>
2022-11-30 14:50:55 +0900

    코드개선(refactoring)

--- src/main/java/com/jpa/jparticle/dto/ArticleForm.java
+++ src/main/java/com/jpa/jparticle/dto/ArticleForm.java
@@ -1,22 +1,15 @@
 package com.jpa.jparticle.dto;
 
 import com.jpa.jparticle.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 public class ArticleForm {
 
     private String title;
     private String content;
-    public ArticleForm(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
     public Article toEntity() {
         return new Article(null, title, content);
