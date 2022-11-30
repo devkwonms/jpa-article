e40c07601bd6055db0d1d39dbca8360739a04a63
devkwonms <alstj1379@nate.com>
2022-11-30 20:09:40 +0900

    CommentDto createCommentDto메소드 추가

--- src/main/java/com/jpa/jparticle/dto/CommentDto.java
+++ src/main/java/com/jpa/jparticle/dto/CommentDto.java
@@ -1,5 +1,6 @@
 package com.jpa.jparticle.dto;
 
+import com.jpa.jparticle.entity.Comment;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,4 +16,13 @@ public class CommentDto {
     private Long articleId;
     private String nickname;
     private String body;
+
+    public static CommentDto createCommentDto(Comment comment) {
+        return new CommentDto(
+                comment.getId(),
+                comment.getArticle().getId(),
+                comment.getNickname(),
+                comment.getBody()
+        );
+    }
 }
