8e6b518f6c838913d055cb719e0b1f24305c28f5
devkwonms <alstj1379@nate.com>
2022-11-30 19:37:19 +0900

    댓글 entity 구현 - @ManyToOne, @JoinColumn

---
+++ src/main/java/com/jpa/jparticle/entity/Comment.java
@@ -0,0 +1,27 @@
+package com.jpa.jparticle.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "article_id")
+    private Article article;
+    @Column
+    private String nickname;
+    @Column
+    private String body;
+}
