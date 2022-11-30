019b04ca158620a84391ea6ece9a5c46045d6aab
devkwonms <alstj1379@nate.com>
2022-11-30 19:30:48 +0900

    index() 테스트

--- src/test/java/com/jpa/jparticle/service/ArticleServiceTest.java
+++ src/test/java/com/jpa/jparticle/service/ArticleServiceTest.java
@@ -1,8 +1,15 @@
 package com.jpa.jparticle.service;
 
+import com.jpa.jparticle.entity.Article;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class ArticleServiceTest {
@@ -12,9 +19,18 @@ public class ArticleServiceTest {
 
     @Test
     void index() {
+
         // 예상
+        Article a = new Article(1L, "가가가가", "1111");
+        Article b = new Article(2L, "나나나나", "2222");
+        Article c = new Article(3L, "다다다다", "3333");
+        List<Article> expected = new ArrayList<Article>(Arrays.asList(a, b, c));
+
         // 실제
+        List<Article> articles = articleService.index();
+
         // 검증
+        assertEquals(expected.toString(), articles.toString());
     }
 
 }
