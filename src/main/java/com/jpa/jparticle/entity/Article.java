b5e8c73e9b2bdbe90d50ef0ac6e1b17c1b17e28a
devkwonms <alstj1379@nate.com>
2022-11-30 15:07:41 +0900

    @NoArgsConstructor, @AllArgsConstructor에 대한 주석기술

--- src/main/java/com/jpa/jparticle/entity/Article.java
+++ src/main/java/com/jpa/jparticle/entity/Article.java
@@ -11,8 +11,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity // DB가 해당 객체를 인식가능하게 함! (해당클래스로 테이블을 생성한다)
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor // ( default constructor 추가 )
+@AllArgsConstructor // ( 해당 클래스의 모든 필드를 매개변수로 갖는 constructor 추가 )
 @ToString
 public class Article {
 
