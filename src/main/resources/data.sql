852cc1ef48a4d8f2a9028b41f1024f1449928ea7
devkwonms <alstj1379@nate.com>
2022-11-30 17:28:00 +0900

    더미 데이터 작성 - data.sql, insert into

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+-- 15강: article 더미 데이터
+INSERT INTO article(id, title, content) VALUES(1, '가가가가', '1111');
+INSERT INTO article(id, title, content) VALUES(2, '나나나나', '2222');
+INSERT INTO article(id, title, content) VALUES(3, '다다다다', '3333');
