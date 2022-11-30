dbd6834e83a08782ad86c9ac628fc3b5f7af75a8
devkwonms <alstj1379@nate.com>
2022-11-30 20:05:43 +0900

    Comment REST컨트롤러 생성 - @RestController

---
+++ src/main/java/com/jpa/jparticle/api/CommentApiController.java
@@ -0,0 +1,21 @@
+package com.jpa.jparticle.api;
+
+import com.jpa.jparticle.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CommentApiController {
+
+    @Autowired
+    private CommentService commentService;
+
+    // 댓글 목록 조회
+
+    // 댓글 생성
+
+    // 댓글 수정
+
+    // 댓글 삭제
+
+}
