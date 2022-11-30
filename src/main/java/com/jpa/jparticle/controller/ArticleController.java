eca8cf71e8fe58108f5e45a4fbdd4ccdef38710a
devkwonms <alstj1379@nate.com>
2022-11-30 17:46:46 +0900

    postmapping update method 생성(수정)

--- src/main/java/com/jpa/jparticle/controller/ArticleController.java
+++ src/main/java/com/jpa/jparticle/controller/ArticleController.java
@@ -82,4 +82,10 @@ public class ArticleController {
         return "articles/edit";
     }
 
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form) {
+        log.info(form.toString());
+        return "";
+    }
+
 }
