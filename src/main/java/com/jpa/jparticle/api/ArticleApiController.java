38180f96c7fc6840b79546c368884dc731abcbd8
devkwonms <alstj1379@nate.com>
2022-11-30 18:19:14 +0900

    RestAPI PATCH 구현

--- src/main/java/com/jpa/jparticle/api/ArticleApiController.java
+++ src/main/java/com/jpa/jparticle/api/ArticleApiController.java
@@ -3,11 +3,15 @@ package com.jpa.jparticle.api;
 import com.jpa.jparticle.dto.ArticleForm;
 import com.jpa.jparticle.entity.Article;
 import com.jpa.jparticle.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 public class ArticleApiController {
 
@@ -32,5 +36,25 @@ public class ArticleApiController {
     }
 
     // PATCH
+    @PatchMapping("/api/articles/{id}")
+    public ResponseEntity<Article> update(@PathVariable Long id,
+                                          @RequestBody ArticleForm dto) {
+        // 1: DTO -> 엔티티
+        Article article = dto.toEntity();
+        log.info("id: {}, article: {}", id, article.toString());
+        // 2: 타겟 조회
+        Article target = articleRepository.findById(id).orElse(null);
+        // 3: 잘못된 요청 처리
+        if (target == null || id != article.getId()) {
+            // 400, 잘못된 요청 응답!
+            log.info("잘못된 요청! id: {}, article: {}", id, article.toString());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+        // 4: 업데이트 및 정상 응답(200)
+        target.patch(article);
+        Article updated = articleRepository.save(target);
+        return ResponseEntity.status(HttpStatus.OK).body(updated);
+    }
+
     // DELETE
 }
