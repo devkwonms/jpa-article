d483f643f24405395b8176735034d1d3123a591b
devkwonms <alstj1379@nate.com>
2022-11-30 17:54:51 +0900

    삭제 완료 메시지 - RedirectAttributes, addFlashAttribute

--- src/main/java/com/jpa/jparticle/controller/ArticleController.java
+++ src/main/java/com/jpa/jparticle/controller/ArticleController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -105,7 +106,8 @@ public class ArticleController {
     }
 
     @GetMapping("/articles/{id}/delete")
-    public String delete(@PathVariable Long id) {
+    public String delete(@PathVariable Long id,
+                         RedirectAttributes rttr) {
         log.info("삭제 요청이 들어왔습니다!!");
         // 1: 삭제 대상을 가져옴
         Article target = articleRepository.findById(id).orElse(null);
@@ -113,6 +115,7 @@ public class ArticleController {
         // 2: 대상을 삭제
         if (target != null) {
             articleRepository.delete(target);
+            rttr.addFlashAttribute("msg", "삭제가 완료되었습니다.");
         }
         // 3: 결과 페이지로 리다이렉트
         return "redirect:/articles";
