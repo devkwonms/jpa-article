2a179cda9d3fd761919a8571d8256a1d0f63b0b2
devkwonms <alstj1379@nate.com>
2022-11-30 19:43:58 +0900

    CommentRepository 구현 - @Query

---
+++ src/main/java/com/jpa/jparticle/repository/CommentRepository.java
@@ -0,0 +1,18 @@
+package com.jpa.jparticle.repository;
+
+import com.jpa.jparticle.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment,Long> {
+    @Query(value =
+            "SELECT * " +
+                    "FROM comment " +
+                    "WHERE article_id = :articleId",
+            nativeQuery = true)
+    List<Comment> findByArticleId(@Param("articleId") Long articleId);
+    List<Comment> findByNickname(String nickname);
+}
