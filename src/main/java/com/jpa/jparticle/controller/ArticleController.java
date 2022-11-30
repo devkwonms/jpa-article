4db3bbc2b3f5b79b5448d52c4c22d038186761ed
devkwonms <alstj1379@nate.com>
2022-11-30 14:34:46 +0900

    repository DI(의존성주입)과 repository를 통한 entity저장(DB로)

--- src/main/java/com/jpa/jparticle/controller/ArticleController.java
+++ src/main/java/com/jpa/jparticle/controller/ArticleController.java
@@ -2,12 +2,17 @@ package com.jpa.jparticle.controller;
 
 import com.jpa.jparticle.dto.ArticleForm;
 import com.jpa.jparticle.entity.Article;
+import com.jpa.jparticle.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
+
+    @Autowired
+    private ArticleRepository articleRepository;
 
     @GetMapping("/articles/new")
     public String newArticleForm() {
@@ -22,6 +27,8 @@ public class ArticleController {
         // 1. Dto를 Entity 변환
         Article article = form.toEntity();
 
+        // 2. Repository에게 Entity를 DB로 저장하게 함
+        Article saved = articleRepository.save(article);
 
         return "";
     }
