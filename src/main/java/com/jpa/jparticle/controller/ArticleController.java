0f9f7a36d8c876e820f633a615b4fdaeb9f6bb29
devkwonms <alstj1379@nate.com>
2022-11-30 14:30:42 +0900

    dto -> entity로 변환하기

--- src/main/java/com/jpa/jparticle/controller/ArticleController.java
+++ src/main/java/com/jpa/jparticle/controller/ArticleController.java
@@ -1,6 +1,7 @@
 package com.jpa.jparticle.controller;
 
 import com.jpa.jparticle.dto.ArticleForm;
+import com.jpa.jparticle.entity.Article;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,11 @@ public class ArticleController {
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
         System.out.println(form.toString());
+
+        // 1. Dto를 Entity 변환
+        Article article = form.toEntity();
+
+
         return "";
     }
 }
