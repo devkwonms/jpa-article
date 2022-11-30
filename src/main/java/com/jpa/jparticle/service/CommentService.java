c9c38a9d26dd1af0b402066fd18be47aca36beb4
devkwonms <alstj1379@nate.com>
2022-11-30 20:16:30 +0900

    반복문 for을 스트림으로 - stream(), map(), collent(), Collectors.toList()

--- src/main/java/com/jpa/jparticle/service/CommentService.java
+++ src/main/java/com/jpa/jparticle/service/CommentService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CommentService {
@@ -19,17 +20,24 @@ public class CommentService {
     private ArticleRepository articleRepository;
 
     public List<CommentDto> comments(Long articleId) {
-        // 조회: 댓글 목록
-        List<Comment> comments = commentRepository.findByArticleId(articleId);
-        // 변환: 엔티티 -> DTO
-        List<CommentDto> dtos = new ArrayList<CommentDto>();
-        for (int i = 0; i < comments.size(); i++) {
-            Comment c = comments.get(i);
-            CommentDto dto = CommentDto.createCommentDto(c);
-            dtos.add(dto);
-        }
-        // 반환
-        return dtos;
+//        // 조회: 댓글 목록
+//        List<Comment> comments = commentRepository.findByArticleId(articleId);
+//        // 변환: 엔티티 -> DTO
+//        List<CommentDto> dtos = new ArrayList<CommentDto>();
+//        for (int i = 0; i < comments.size(); i++) {
+//            Comment c = comments.get(i);
+//            CommentDto dto = CommentDto.createCommentDto(c);
+//            dtos.add(dto);
+//        }
+//        // 반환
+//        return dtos;
+
+        // 반환(for문 -> stream으로 리팩토링)
+        return commentRepository.findByArticleId(articleId)
+                .stream()
+                .map(comment -> CommentDto.createCommentDto(comment))
+                .collect(Collectors.toList());
+
 
     }
 }
