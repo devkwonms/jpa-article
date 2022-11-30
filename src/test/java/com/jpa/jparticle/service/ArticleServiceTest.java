8a105d6c66acd92a416149fcd529d7febe887aa2
devkwonms <alstj1379@nate.com>
2022-11-30 19:29:41 +0900

    테스트 코드 틀잡기 - @Test, @SpringBootTest

---
+++ src/test/java/com/jpa/jparticle/service/ArticleServiceTest.java
@@ -0,0 +1,20 @@
+package com.jpa.jparticle.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ArticleServiceTest {
+
+    @Autowired
+    ArticleService articleService;
+
+    @Test
+    void index() {
+        // 예상
+        // 실제
+        // 검증
+    }
+
+}
