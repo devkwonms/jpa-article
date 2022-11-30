17ba6d89ce16fd0e08cde3eff419a0d7af67b423
devkwonms <alstj1379@nate.com>
2022-11-30 20:10:24 +0900

    CommentService comments 메서드 추가

--- src/main/java/com/jpa/jparticle/service/CommentService.java
+++ src/main/java/com/jpa/jparticle/service/CommentService.java
@@ -1,9 +1,14 @@
 package com.jpa.jparticle.service;
 
+import com.jpa.jparticle.dto.CommentDto;
+import com.jpa.jparticle.entity.Comment;
 import com.jpa.jparticle.repository.ArticleRepository;
 import com.jpa.jparticle.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CommentService {
@@ -12,4 +17,19 @@ public class CommentService {
     private CommentRepository commentRepository;
     @Autowired
     private ArticleRepository articleRepository;
+
+    public List<CommentDto> comments(Long articleId) {
+        // 조회: 댓글 목록
+        List<Comment> comments = commentRepository.findByArticleId(articleId);
+        // 변환: 엔티티 -> DTO
+        List<CommentDto> dtos = new ArrayList<CommentDto>();
+        for (int i = 0; i < comments.size(); i++) {
+            Comment c = comments.get(i);
+            CommentDto dto = CommentDto.createCommentDto(c);
+            dtos.add(dto);
+        }
+        // 반환
+        return dtos;
+
+    }
 }
