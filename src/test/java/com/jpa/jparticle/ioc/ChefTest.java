6e27940b9f2166a47ee08a01e42c9c2d7a559627
devkwonms <alstj1379@nate.com>
2022-12-02 14:55:37 +0900

    돈가스 요리하기 테스트

---
+++ src/test/java/com/jpa/jparticle/ioc/ChefTest.java
@@ -0,0 +1,23 @@
+package com.jpa.jparticle.ioc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChefTest {
+
+    @Test
+    void 돈가스_요리하기(){
+        // 준비
+        Chef chef = new Chef();
+        String menu = "돈가스";
+        // 수행
+        String food = chef.cook(menu);
+        // 예상
+        String expected = "한돈 등심으로 만든 돈가스";
+        // 검증
+        assertEquals(expected,food);
+
+        System.out.println(food);
+    }
+}
