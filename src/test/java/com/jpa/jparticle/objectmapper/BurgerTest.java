b2cc55e0dba7b3f1b8f4a2c72df8dcd7bf322f17
devkwonms <alstj1379@nate.com>
2022-12-02 17:53:17 +0900

    JSON을 객체로 - readValue(), @NoArgsConstructor

--- src/test/java/com/jpa/jparticle/objectmapper/BurgerTest.java
+++ src/test/java/com/jpa/jparticle/objectmapper/BurgerTest.java
@@ -16,7 +16,7 @@ class BurgerTest {
     public void 자바_객체를_JSON으로_변환() throws JsonProcessingException {
         // 준비
         ObjectMapper objectMapper = new ObjectMapper();
-        List<String> ingredients = Arrays.asList("통새우 패티","순쇠고기 패티","토마토","스파이시 어니언 소스");
+        List<String> ingredients = Arrays.asList("통새우 패티", "순쇠고기 패티", "토마토", "스파이시 어니언 소스");
         Burger burger = new Burger("맥도날드 슈비버거", 5500, ingredients);
         // 수행
         String json = objectMapper.writeValueAsString(burger);
@@ -25,11 +25,32 @@ class BurgerTest {
         String expected = "{\"name\":\"맥도날드 슈비버거\",\"price\":5500,\"ingredients\":[\"통새우 패티\",\"순쇠고기 패티\",\"토마토\",\"스파이시 어니언 소스\"]}";
 
         // 검증
-        assertEquals(expected,json);
+        assertEquals(expected, json);
 //        System.out.println(json);
 
         JsonNode JsonNode = objectMapper.readTree(json);
         System.out.println(JsonNode.toPrettyString());
     }
 
+    @Test
+    public void JSON을_자바_객체로_변환() throws JsonProcessingException {
+        // 준비
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = "{\"name\":\"맥도날드 슈비버거\",\"price\":5500,\"ingredients\":[\"통새우 패티\",\"순쇠고기 패티\",\"토마토\",\"스파이시 어니언 소스\"]}";
+
+        // 수행
+        Burger burger = objectMapper.readValue(json, Burger.class);
+
+        // 예상
+        List<String> ingredients = Arrays.asList("통새우 패티", "순쇠고기 패티", "토마토", "스파이시 어니언 소스");
+        Burger expected = new Burger("맥도날드 슈비버거", 5500, ingredients);
+
+        // 검증
+        assertEquals(expected.toString(),burger.toString());
+        System.out.println(json);
+        System.out.println(burger.toString());
+
+
+    }
 }
