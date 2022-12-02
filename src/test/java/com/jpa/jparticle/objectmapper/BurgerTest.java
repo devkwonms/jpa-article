36808d293e8466a8017d6550b36f5f71a3caf570
devkwonms <alstj1379@nate.com>
2022-12-02 18:08:15 +0900

    JSON 직접 만들기 - createObjectNode(), ObjectNode, ArrayNode

--- src/test/java/com/jpa/jparticle/objectmapper/BurgerTest.java
+++ src/test/java/com/jpa/jparticle/objectmapper/BurgerTest.java
@@ -3,6 +3,8 @@ package com.jpa.jparticle.objectmapper;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -37,7 +39,28 @@ class BurgerTest {
         // 준비
 
         ObjectMapper objectMapper = new ObjectMapper();
-        String json = "{\"name\":\"맥도날드 슈비버거\",\"price\":5500,\"ingredients\":[\"통새우 패티\",\"순쇠고기 패티\",\"토마토\",\"스파이시 어니언 소스\"]}";
+//        String json = "{\"name\":\"맥도날드 슈비버거\",\"price\":5500,\"ingredients\":[\"통새우 패티\",\"순쇠고기 패티\",\"토마토\",\"스파이시 어니언 소스\"]}";
+        /*
+        {
+          "name" : "맥도날드 슈비버거",
+          "price" : 5500,
+          "ingredients" : [ "통새우 패티", "순쇠고기 패티", "토마토", "스파이시 어니언 소스" ]
+        }
+        */
+
+        ObjectNode objectNode = objectMapper.createObjectNode();
+        objectNode.put("name","맥도날드 슈비버거");
+        objectNode.put("price",5500);
+
+        ArrayNode arrayNode = objectMapper.createArrayNode();
+        arrayNode.add("통새우 패티");
+        arrayNode.add("순쇠고기 패티");
+        arrayNode.add("토마토");
+        arrayNode.add("스파이시 어니언 소스");
+
+        objectNode.set("ingredients",arrayNode);
+
+        String json = objectNode.toString();
 
         // 수행
         Burger burger = objectMapper.readValue(json, Burger.class);
