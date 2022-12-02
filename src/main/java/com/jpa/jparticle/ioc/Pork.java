ce2e367696cf72e0aa61ac2919179a4c64a0ae5a
devkwonms <alstj1379@nate.com>
2022-12-02 14:56:04 +0900

    돈가스 요리하기 테스트

---
+++ src/main/java/com/jpa/jparticle/ioc/Pork.java
@@ -0,0 +1,13 @@
+package com.jpa.jparticle.ioc;
+
+public class Pork {
+    private  String name;
+
+    public Pork(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
